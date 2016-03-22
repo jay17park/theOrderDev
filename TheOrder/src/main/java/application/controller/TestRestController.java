466476d0bf1678d6e9ff612659c01ffc29ade137
jay17.park(박재영) <susia7179@gmail.com>
2016-03-22 16:49:09 +0900

    최초 커밋

---
+++ TheOrder/src/main/java/application/controller/TestRestController.java
@@ -0,0 +1,13 @@
+package application.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestRestController {
+	
+	@RequestMapping("/testRestApi")
+	public String test(){		
+		return "Hello World";		 
+	}
+}
